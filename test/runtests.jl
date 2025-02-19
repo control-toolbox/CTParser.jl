a6767b2b9b9e45db09a10e73591d698f3303a932
Jean-Baptiste Caillau <jean-baptiste.caillau@univ-cotedazur.fr>
2025-02-19 17:10:52 +0100

    onepass

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,7 @@ using CTParser
 
 #
 @testset verbose = true showtiming = true "CTParser tests" begin
-    for name in (:aqua, :default)
+	for name in (:default, ) #(:aqua, :default)
         @testset "$(name)" begin
             test_name = Symbol(:test_, name)
             include("$(test_name).jl")
