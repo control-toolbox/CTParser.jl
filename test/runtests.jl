c1fed84de5aa0399fb6bcadc60587a5065ea5325
Jean-Baptiste Caillau <jean-baptiste.caillau@univ-cotedazur.fr>
2025-02-20 18:44:26 +0100

    passed utils tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,11 @@
-using CTParser
+using CTParser: CTParser, subs, replace_call, has, constraint_type, @def 
 using Test
 using Aqua
 #import CTModels # debug: should eventually be OptimalControl (that will reexport CTModels func. primitives)
 
 #
 @testset verbose = true showtiming = true "CTParser tests" begin
-	for name in (:default, ) #(:aqua, :default)
+	for name in (:aqua, :utils)
         @testset "$(name)" begin
             test_name = Symbol(:test_, name)
             include("$(test_name).jl")
--- test/test_default.jl
+++
@@ -1,4 +0,0 @@
-# test_default
-function test_default()
-    @test nothing isa Nothing
-end
