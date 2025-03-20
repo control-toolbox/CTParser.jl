7aaad1f6ea89596daa50ade6cf977fd31fa7fe9d
Jean-Baptiste Caillau <jean-baptiste.caillau@univ-cotedazur.fr>
2025-03-20 22:17:13 +0100

    refined imports

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using Test
 using Aqua
 import CTParser: CTParser, subs, replace_call, has, constraint_type, set_prefix, @def
-import CTBase: ParsingError
+import CTBase: CTBase, ParsingError
 import CTModels: CTModels, initial_time, final_time, time_name, variable_dimension, variable_components, variable_name, state_dimension, state_components, state_name, control_dimension, control_components, control_name, constraint, dynamics, mayer, lagrange, criterion, Model
 
 #
