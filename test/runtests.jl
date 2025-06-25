1dbefd50b13a7f74642ef7c7c8f91cffa86006d1
Jean-Baptiste Caillau <jean-baptiste.caillau@univ-cotedazur.fr>
2025-06-25 23:49:42 +0200

    tests exa OK on local GPU (A100); pushing to test on moonshot

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,9 +13,9 @@ using Interpolations
 macro ignore(e) return :() end
 
 @testset verbose = true showtiming = true "CTParser tests" begin
-    for name ∈ (:aqua, :utils, :onepass_fun, :onepass_exa)
+    #for name ∈ (:aqua, :utils, :onepass_fun, :onepass_exa)
 	#for name ∈ (:onepass_fun,)
-	#for name ∈ (:onepass_exa,)
+	for name ∈ (:onepass_exa,)
         @testset "$(name)" begin
             test_name = Symbol(:test_, name)
             include("$(test_name).jl")
