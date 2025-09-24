0f7f471b90d0c7c2fafdc24657b77082ac6bf34c
Jean-Baptiste Caillau <jean-baptiste.caillau@univ-cotedazur.fr>
2025-09-24 11:58:06 +0200

    subs5: test OK

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 using Test
 using Aqua
-import CTParser: CTParser, subs, subs2, subs3, subs4, replace_call, has, concat, constraint_type, @def, prefix_fun, prefix_fun!, prefix_exa, prefix_exa!, e_prefix, e_prefix!, activate_backend, deactivate_backend, is_active_backend, @def_exa # todo: remove __default... (as soon as discretise_exa has been moved to CTDirect)
+import CTParser: CTParser, subs, subs2, subs3, subs4, subs5, replace_call, has, concat, constraint_type, @def, prefix_fun, prefix_fun!, prefix_exa, prefix_exa!, e_prefix, e_prefix!, activate_backend, deactivate_backend, is_active_backend, @def_exa # todo: remove __default... (as soon as discretise_exa has been moved to CTDirect)
 import CTBase: CTBase, ParsingError, UnauthorizedCall
 import CTModels: CTModels, initial_time, final_time, time_name, variable_dimension, variable_components, variable_name, state_dimension, state_components, state_name, control_dimension, control_components, control_name, constraint, dynamics, mayer, lagrange, criterion, Model, get_build_examodel
 import ExaModels
