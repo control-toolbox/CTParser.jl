30fa71122191a8adc62563f75116662e4785e2fe
Jean-Baptiste Caillau <jean-baptiste.caillau@univ-cotedazur.fr>
2025-12-21 17:48:50 +0100

    tempo to save basic first exa tries

--- test/test_onepass_exa.jl
+++ test/test_onepass_exa.jl
@@ -3,7 +3,49 @@
 
 activate_backend(:exa) # nota bene: needs to be executed before @def are expanded
 
-# mock up of CTDirect.discretise for tests
+# Auxiliary functions
+# debug: add tests on these, and export them (both from CTParser and from OC)
+# try: just redefine Base.zero and Base.adjoint; may be add promote and convert 
+
+import Base: zero, adjoint, *
+import LinearAlgebra: dot, Adjoint
+
+zero(x::T) where {T <: ExaModels.AbstractNode} = 0
+
+adjoint(x::ExaModels.AbstractNode) = x
+
+## Convert Number to AbstractNode using Null
+Base.convert(::Type{ExaModels.AbstractNode}, x::Number) = ExaModels.Null(x)
+#
+## Also handle the identity conversion
+#Base.convert(::Type{T}, x::T) where {T<:ExaModels.AbstractNode} = x
+#
+## If you need more specific typing:
+#Base.convert(::Type{ExaModels.Null{T}}, x::Number) where {T} = 
+#    ExaModels.Null{typeof(x)}(x)
+
+# works to define matrices (of vectors) with symbolic entries
+Base.promote_rule(::Type{<:ExaModels.AbstractNode}, ::Type{<:Number}) = ExaModels.AbstractNode
+
+function dot(v::Vector{T}, x::Vector{S}) where {T, S <: ExaModels.AbstractNode}
+    @assert length(v) == length(x)
+    return sum(v .* x)
+end
+
+function *(A::Matrix{T}, x::Vector{S}) where {T, S <: ExaModels.AbstractNode}
+    m, n = size(A)
+    @assert n == length(x)
+    return [dot(A[i, :], x) for i in 1:m]
+end
+
+function *(p::Adjoint{T, Vector{T}}, A::Matrix{S}) where {T <: ExaModels.AbstractNode, S}
+    m, n = size(A)
+    @assert m == length(p)
+    return [p * A[:, j] for j in 1:n]'
+end
+
+# Mock up of CTDirect.discretise for tests
+
 function discretise_exa(
     ocp;
     scheme=CTParser.__default_scheme_exa(),
@@ -31,6 +73,8 @@ function discretise_exa_full(
         scheme=scheme, grid_size=grid_size, backend=backend, init=init, base_type=base_type
     )
 end
+
+# Tests
 
 function test_onepass_exa()
     #l_scheme = [:euler, :euler_implicit, :midpoint, :trapeze]
@@ -1795,9 +1839,9 @@ function __test_onepass_exa(
             x ∈ R², state
             u ∈ R, control
             x(0) == x0
-            ∂(x₁)(t) == A[1, :]' * x(t) + u(t) * B[1]
+            ∂(x₁)(t) == dot(A[1, :], x(t)) + u(t) * B[1]
             #∂(x₁)(t) == x₂(t)
-            ∂(x₂)(t) == A[2, :]' * x(t) + u(t) * B[2]
+            ∂(x₂)(t) == dot(A[2, :], x(t)) + u(t) * B[2]
             #∂(x₂)(t) == -x₁(t) + u(t) 
             0.5∫( x(t)' * Q * x(t) + u(t)' * R * u(t) ) → min
             #0.5∫( x₁(t)^2 + x₂(t)^2 + u(t)^2 ) → min
